9bf0e6909c741a4d0278e0b7c0b77100ec7f369c
gayashan4lk <gayashan4lk@gmail.com>
2024-05-26 02:45:50 +0530

    add some inputs

--- src/users/Users.tsx
+++ src/users/Users.tsx
@@ -1,3 +1,22 @@
+import { Box, Button, Input, Stack } from '@mui/joy'
+
 export function Users() {
-  return <div>Users</div>
+  return (
+    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+      <form action="">
+        <Stack spacing={1}>
+          <Input placeholder="Type in here…" size="sm" />
+          <Stack spacing={1} direction={'row'}>
+            <Button size="sm">Save</Button>
+            <Button disabled size="sm">
+              Save
+            </Button>
+            <Button loading size="sm">
+              Loading
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+    </Box>
+  )
 }
