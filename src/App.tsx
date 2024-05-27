0af5cc214f8dd3ff7bf6c9c9949783649961389a
gayashan4lk <gayashan4lk@gmail.com>
2024-05-27 19:30:07 +0530

    build just the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
-import { Users } from './users/Users'
+import { AddUser } from './pages/add-user/AddUser.tsx'
 
 const queryClient = new QueryClient()
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Users />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <AddUser />
+      </LocalizationProvider>
     </QueryClientProvider>
   )
 }
